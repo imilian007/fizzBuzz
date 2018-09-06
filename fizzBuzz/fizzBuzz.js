8e8ffdb342b527dd73c8cf8226e833f308da3277
imilian007 <43039654+imilian007@users.noreply.github.com>
2018-09-06 14:44:28 -0500

    Functional While Loop
    Doesn't print to the site page

---
+++ fizzBuzz/fizzBuzz.js
@@ -0,0 +1,21 @@
+
+
+let i = 0;
+
+while(i <= 100) 
+{
+	if(i % 3 == 0)
+		{
+			
+			console.log("fizz");
+		}
+	if(i % 5 == 0)
+		{
+			console.log("buzz");
+		}
+	else
+		{
+			console.log(" ");
+		}
+	i++;
+}
